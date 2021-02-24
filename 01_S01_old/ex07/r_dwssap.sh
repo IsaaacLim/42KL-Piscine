4fa345b79563679f71cc834a6069c167421d6fa3
Jinq jia Lim <jinlim@u91z05s03.42kl.edu.my>
2021-02-24 11:21:39 +0800

    S01_old removed

--- 01_S01_old/ex07/r_dwssap.sh
+++
@@ -1,9 +0,0 @@
-cat /etc/passwd \
-| sed '/^#/d' \
-| awk 'NR %2 == 0' \
-| sed 's/:.*//g' | rev \
-| sort -r \
-| awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' \
-| tr '\n' ' ' | sed 's/ /, /g' \
-| sed 's/, $/./g' \
-| tr -d '\n'  
