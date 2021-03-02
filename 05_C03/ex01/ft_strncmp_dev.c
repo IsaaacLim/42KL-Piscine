f5ebc6f8e547d2ee7708df3a63ee960bc40fe720
Jinq jia Lim <jinlim@u91z05s03.42kl.edu.my>
2021-03-02 12:58:42 +0800

    ex01 realized why

--- 05_C03/ex01/ft_strncmp_dev.c
+++ 05_C03/ex01/ft_strncmp_dev.c
@@ -6,7 +6,7 @@
 /*   By: jinlim <jinlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 18:22:52 by jinlim            #+#    #+#             */
-/*   Updated: 2021/03/02 12:16:23 by jinlim           ###   ########.fr       */
+/*   Updated: 2021/03/02 12:58:22 by jinlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_strncmp(char *s1, char *s2, unsigned int n)
 
 	i = 0;
 	if (n == 0)
-		return (0);
+		return (0); //else return will still be a comparison between 1st char
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return (*(unsigned char*)&s1[i] - *(unsigned char*)&s2[i]);
@@ -34,8 +34,8 @@ int		main(void)
 
 	str1 = "hello";
 	str2 = "Hello";
-	str3 = "helloo";
-	n = 5; //stops the comparison
+	str3 = "aelloo";
+	n = 7; //stops the comparison
 	printf("ft, str1-str3: %d\n", ft_strncmp(str1, str3, n));
 	printf("ori, str1-str3: %d\n", strncmp(str1, str3, n));
 }
